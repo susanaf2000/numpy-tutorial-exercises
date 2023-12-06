8916aad3b7a34a614d210588156cdb1c488842b3
Susana Florêncio <101977568+susanaf2000@users.noreply.github.com>
2023-12-06 01:35:44 +0000

    12

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 import numpy as np
+"""
+Ex17:
+arr = np.ones([5, 5])
+newarr = np.pad(arr, pad_width=1, mode='constant', constant_values=0)
+print(newarr)
 
-print(np.__version__)
-x = np.zeros(10)
-print(x)
-print(x.size)
-print(x.itemsize)
-total_memory_size = x.size * x.itemsize
-print(total_memory_size)
-print(np.info(np.add))
+Ex18:
+print("0 * np.nan:", 0 * np.nan)
+print("np.nan == np.nan:", np.nan == np.nan)
+print("np.inf > np.nan:", np.inf > np.nan)
+print("np.nan - np.nan:", np.nan - np.nan)
+print("np.nan in set([np.nan]):", np.nan in set([np.nan]))
+print("0.3 == 3 * 0.1:", 0.3 == 3 * 0.1)
+
+
+Ex19:
+matrix = np.arange(9).reshape(3, 3)
+diagonal_values = np.diag(matrix)
+print(diagonal_values)
+
+
+
+Ex20:
+matrix = np.ones((8, 8))
+
+matrix[::2, ::2] = 0  # Even rows, even columns
+matrix[1::2, 1::2] = 0  # Odd rows, odd columns
+matrix[::2, 1::2] = 1  # Even rows, odd columns
+matrix[1::2, ::2] = 1 # Odd rows, even columns
+print(matrix)
+
+"""
